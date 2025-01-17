88a761033b60e07cdd646f2c422e4566b676be5b
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-01-17 23:06:08 +0500

    script.js

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+let quizCategory = 'programming'
+
+const getRandomQuestion = () => {
+  const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase());
+  console.log(categoryQuestions);
+}
+getRandomQuestion()
