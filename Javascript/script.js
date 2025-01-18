ad93533bbae1680ffed6bac7e3313284f867ddb4
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-01-18 14:23:58 +0500

    random question

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,41 @@
-let quizCategory = 'programming'
+const answerOptions = document.querySelector('.answer-options');
+const nextQuestionBtn = document.querySelector('.next-question-btn')
 
+let quizCategory = 'programming';
+let currentQuestion = null
+
+// Fetch a random from pased on the selected category
 const getRandomQuestion = () => {
-  const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase());
-  console.log(categoryQuestions);
+  const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
+
+  const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)]
+  return randomQuestion;
 }
-getRandomQuestion()
+const handleAnswer = (option, answerIndex) => {
+  const isCorrect = currentQuestion
+}
+
+// Render the current question and its options in the quiz
+const renderQuestion = () => {
+  currentQuestion = getRandomQuestion();
+  if(!currentQuestion) return;
+  console.log(currentQuestion);
+
+  // Update the UI
+  answerOptions.innerHTML = '';
+  document.querySelector('.question-text').textContent = currentQuestion.question;
+
+
+  // Create option <li> elements and append them, and add click event listeners
+    currentQuestion.options.forEach((option, index) => {
+    const li = document.createElement('li');
+    li.classList.add('answer-option');
+    li.textContent = option;
+    answerOptions.appendChild(li);
+    li.addEventListener('click', () => handleAnswer(li, index ))
+  })
+}
+
+renderQuestion();
+
+nextQuestionBtn.addEventListener('click', renderQuestion)
